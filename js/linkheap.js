e7618e12d72629ae71007bcfac92b5328e2d5680
Siim Orasmäe <siim.orasmae@gmail.com>
2014-11-10 11:37:03 +0200

    Fix: JSON response not an array
    Fixed a bug where if the JSON response was single item, it wasn't an
    array, so the ng-repeat repeated the wrong stuff.

--- js/linkheap.js
+++ js/linkheap.js
@@ -2,7 +2,7 @@
 var app = angular.module('linkheap', ['ngSanitize']);
 
 app.controller('appController', function($scope, $http) {
-	getItems(0,'siimorasmae');
+	getItems(0,'siimorasmae'); // init
 
 	$scope.refreshItems = function(type,user) {
 		getItems(type,user);
@@ -19,15 +19,17 @@ app.controller('appController', function($scope, $http) {
 		$scope.doneLoading = false;
 		$scope.items = [];
 
-		if (type == 0) {
+		if (type == 0)
 			url = 'https://www.readability.com/'+user+'/latest/feed';
-		}
-		else {
+		else
 			url = 'https://www.readability.com/'+user+'/favorites/feed';
-		}
 
 		$http.jsonp('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url="'+url+'"&format=json&callback=JSON_CALLBACK').success(function(data) {
-			$scope.items = data.query.results.rss.channel.item;
+			if (data.query.results.rss.channel.item.length > 0)
+				$scope.items = data.query.results.rss.channel.item;
+			else
+				$scope.items = [data.query.results.rss.channel.item]; // if returns 1 item
+
 			$scope.doneLoading = true;
 		});
 	};
